ca66ebdf35f72b2a25330b0d58f272af210f5bf7
Arif <arifali123454150@gmail.com>
2018-12-22 16:45:27 +0530

    editing first time

--- main_game.py
+++ main_game.py
@@ -1,22 +1,31 @@
+from datetime import datetime
+start_time = datetime.now()
 from pygame import *
 import random
 from rockets.player import *
-
+WIDTH = 600
+HEIGHT = 720
+#repository 2 check
 
 class Game:
     def __init__(self):
         init()
         mixer.init()
-        self.screen = display.set_mode((500,720))
+        self.screen = display.set_mode((WIDTH,HEIGHT))
         display.set_caption("rocket genetic algorithm")
         self.animation_time = time.Clock()
+        self.dna = []
         self.running = True
+        self.counter = counter
 
     def new(self):
         self.all_sprites = sprite.Group()
         self.player = sprite.Group()
-        for x in range(50):
-            x = Player()
+        self.target = Target()
+        self.all_sprites.add(self.target)
+        for x in range(100):
+            x = Player(self)
+            self.dna.append(x)
             self.all_sprites.add(x)
             self.player.add(x)
         self.run()
@@ -29,8 +38,10 @@ class Game:
             self.update()
             self.animation_time.tick(60)
 
+
     def update(self):
         self.all_sprites.update()
+
 
     def events(self):
         for e in event.get():
@@ -38,6 +49,9 @@ class Game:
                 if self.playing:
                     self.playing = False
                 self.running = False
+                for x in self.dna:
+                    print(x.pos)
+
 
     def draw(self):
         self.screen.fill((0,0,0))
--- player.py
+++ player.py
@@ -1,19 +1,30 @@
 import math
 from pygame import *
 import random
-
+WIDTH = 600
+HEIGHT = 720
+counter = 0
 vec = math.Vector2
 
+
 class Player(sprite.Sprite):
-    def __init__(self):
+    def __init__(self,game):
         sprite.Sprite.__init__(self)
         self.image = Surface((5,40))
         self.image.fill((100,100,100))
+        self.game = game
+        # draw.circle(self.image,(100,100,100),)
         self.rect = self.image.get_rect()
         self.rect.center = (250,700)
         self.pos = vec(250,570)
         self.vel = vec(0,0)
         self.acc = vec(0,0)
+
+    # def respawn(self):
+    #     global  counter
+    #     counter = 0
+
+
 
     def update(self):
         changerx = random.randrange(-900,+900)
@@ -24,6 +35,25 @@ class Player(sprite.Sprite):
         self.vel += self.acc
         self.pos += self.vel + 0.5*self.acc
         self.rect.midbottom = self.pos
-
+        if self.rect.x>WIDTH or self.rect.x<0 or self.rect.y>HEIGHT or self.rect.y<0:
+            self.kill()
+            global counter
+            counter= counter+1
+            print(counter)
+            if counter > 99:
+                counter= 0
+                self.game.new()
         # changer = random.randrange(-5, 0)
         # self.rect.y += changer
+
+
+class Target(sprite.Sprite):
+    def __init__(self):
+        sprite.Sprite.__init__(self)
+        self.image = Surface((20,20))
+        self.image.fill((255,255,0))
+        self.rect = self.image.get_rect()
+        self.rect.center = (250,100)
+
+    def update(self):
+        pass
